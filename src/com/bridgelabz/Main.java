6fec84770b5e9f933676db1ec1c236c71e88d7e2
sumithanvat <skhanvat@gmail.com>
2023-03-31 13:49:04 +0530

    Added:As a User need to enter a valid Last Name - Last name starts

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your first name: ");
-        String firstName = scanner.nextLine();
+        System.out.print("Enter your Last name: ");
+        String LastName = scanner.nextLine();
 
-        if (UserRegistration.validateFirstName(firstName)) {
-            System.out.println("First name is valid!");
+        if (UserRegistration.validateLastName(LastName)) {
+            System.out.println("Last name is valid!");
         } else {
-            System.out.println("Please enter a valid first name.");
+            System.out.println("Please enter a valid Last name.");
         }
     }
 }
--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -2,14 +2,14 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 public class UserRegistration {
 
-  public static boolean validateFirstName(String firstName) {
+  public static boolean validateLastName(String firstName) {
         /**
          * Validates the first name input by the user.
          *
          * @param firstName String, the user's first name input
          * @return boolean, true if the first name is valid, false otherwise
          */
-        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
         return pattern.matcher(firstName).matches();
     }
 }
